ecd7d056b5b8a05c83f2cf2361e6a65f8e776323
Danny Pr <47109543+dannpr@users.noreply.github.com>
2024-02-17 20:15:27 +0100

    change to test backend

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-
 // GET home page
-router.get("/", (req, res) => {
+/* router.get("/", (req, res) => {
   res.status(200).send("Server is healthy");
 });
+ */
 
+const MAGIC_PUBLISHABLE_KEY = process.env.MAGIC_PUBLIC;
+
+router.get("/", (req, res) => {
+  res.render("index", { title: "Magic Apple Store 🍎", MAGIC_PUBLISHABLE_KEY });
+});
 module.exports = router;
